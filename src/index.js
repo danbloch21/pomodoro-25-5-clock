61481fe9e27f0ec9b4a838bc975d8f1e6efd8c28
Dan Bloch <danjobloch@gmail.com>
2022-04-11 20:55:10 +0200

    first stage building pomodoro clock

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+function App() {
+  const [displayTime, setDisplayTime] = React.useState(25 * 60);
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
+
+  return (
+    <div>
+      <Length
+        title={"Break Length"}
+        changeTime={null}
+        type={"break"}
+        time={null}
+        formatTime={formatTime}
+      />
+      <h1>{formatTime(displayTime)}</h1>
+    </div>
+  );
+}
+
+function Length({ title, changeTime, type, time, formatTime }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className="time-sets"></div>
+      <button className="btn-small deep-purple lighten-2">
+        <i className="material-icons">arrow_downward</i>
+      </button>
+      <h3>{formatTime(time)}</h3>
+      <button className="btn-small deep-purple lighten-2">
+        <i className="material-icons">arrow_upward</i>
+      </button>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
